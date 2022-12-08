a623377622943fe1b17904609c9237ef2f20e623
Jocelle Bautista <jcllebautista@gmail.com>
2022-12-08 16:27:08 -0500

    Create promise function with fetched data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,10 @@
 // Your fetch requests will live here!
 const fetchUserData = () => {
-        fetch("https://fitlit-api.herokuapp.com/api/v1/users")
-        .then((response) => response.json())
-        .then(users => {
-            console.log(users)
-        })
-    }
+        return [fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+          .then((response) => response.json())]
+        }
+  
+// console.log('I will be a fetch request!', fetchUserData())
 
-console.log('I will be a fetch request!')
-
-export default fetchUserData()
+export default fetchUserData
 // export {promiseAll}
--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,7 @@ import userData from './data/users';
 import User from './User';
 import UserRepository from './UserRepository';
 import fetchUserData from "./apiCalls.js"
+import promiseAll from './apiCalls.js'
 
 const userName = document.querySelector('#username') 
 const name = document.querySelector('#name') 
@@ -18,10 +19,18 @@ let user
 let userRepo
 let currentUser
 
-user = new User(userData[Math.floor(Math.random() * userData.length)])
-userRepo = new UserRepository(userData)
+// console.log('here', fetchUserData())
+Promise.all(fetchUserData()) //pass an array into the args --> 
+    .then(data => {
+      console.log('data', data[0].userData) //
+      const userDataArray = data[0].userData //array of data object --> class instance obj
+      const usersArray = userDataArray.map(userObj => new User(userObj)) //
 
-  console.log('here', user)
+      user = new User(userData[Math.floor(Math.random() * userData.length)])
+      userRepo = new UserRepository(usersArray)
+      console.log('userRepo', userRepo)
+    })
+
   window.addEventListener('load', () => {
     user
     displayName()
