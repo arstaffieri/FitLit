746162109e23cf209c6d58c9f52ec9222f10b9c2
Timothy Pendarvis <trpendarvis@gmail.com>
2022-12-08 16:48:09 -0700

    Start Hydration class and test

---
+++ src/Hydration.js
@@ -0,0 +1,9 @@
+class Hydration{
+    constructor(){
+        // this.userID = userID
+        // this.ouncesConsumedDay = 
+        // this.ouncesConsumedWeek =
+    }
+}
+
+export default Hydration
