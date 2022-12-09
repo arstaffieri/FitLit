e50422f5baba1aade12c2743b35bc7e4f1f91766
Jocelle Bautista <jcllebautista@gmail.com>
2022-12-09 17:14:53 -0500

    Display user data to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,6 @@ import UserRepository from './UserRepository';
 import fetchData from "./apiCalls.js"
 import promiseAll from './apiCalls.js'
 
-
 const sleepData = document.querySelector('#sleep-data') 
 const userName = document.querySelector('#username') 
 const name = document.querySelector('#name') 
@@ -30,68 +29,51 @@ const getData = () => {
       users = data[0].userData 
       user = users[Math.floor(Math.random() * users.length)]
       currentUser = new User(user) 
-      console.log('current user', currentUser)
-      return currentUser
-    })}
-      // return users
-    
-      console.log('get data', getData)
-    window.addEventListener('load', () => {
-      user
-      promiseAll
-      // getRandomUser()
-      getData()
-      displayName()
-      displayInfoToDom()
-      stepGoalDisplay()
-      friendNames()
+      
+    displayInfoToDom()
     })
+  }
     
-    // user = new User(userData[Math.floor(Math.random() * userData.length)])
-    // userRepo = new UserRepository(usersArray)
-    // console.log('userRepo', userRepo)
-  //   function getRandomUser(){
-
-  //   const newUser = users(user => new User(users[Math.floor(Math.random() * users.length)])) 
-
-  //   userRepo = new UserRepository(usersArray)
-  //   console.log('userRepo', userRepo)
-  //   return newUser
-  // }
-
-function displayName() {
-  userName.innerHTML = `Welcome, ${currentUser.showFirstName()}!`
-   }
-
-   function displaySleepData(){
-    // sleepData.innerHTML += 
-    // `<li> You slept ${hoursSlept()}hours! yout sleep quality was ${sleepQuality(example)}</li>
-    // <li>You slept ${hoursSlept(example)}hours this week! yout sleep quality was ${sleepQuality}</li>
-    // <li>$ Your all-time average sleep quality is ${allTimeQuality(example)} and your all-time average number of hours slept is ${allTimeHoursSlept(example)}</li>
+window.addEventListener('load', () => {
+  user
+  promiseAll
+  getData()
+  displayInfoToDom()
+})
     
-   }
-
 function displayInfoToDom() {
-    name.innerHTML = `Name: ${user.name}`
-    email.innerHTML = `Email: ${user.email}`
-    address.innerHTML = `Address: ${user.address}`
-    strideLength.innerHTML = `Your Stride Length: ${user.strideLength}`
-    userStepGoal.innerHTML = `Your Step Goal: ${user.dailyStepGoal}`
-    // friendsData.innerHTML = `Your Friends: ${user.friends.showFirstName()}`
+  displayUserInfo()
 }
 
-function stepGoalDisplay() {     averageStepGoal.innerHTML = `Your step goal is ${user.dailyStepGoal} steps. The average step goal is ${userRepo.getAverageStepGoal()}.`
+function displayUserInfo() {
+  userName.innerHTML = `Welcome, ${currentUser.showFirstName()}!`
+  name.innerHTML = `Name: ${currentUser.name}`
+  email.innerHTML = `Email: ${currentUser.email}`
+  address.innerHTML = `Address: ${currentUser.address}`
+  strideLength.innerHTML = `Your Stride Length: ${currentUser.strideLength}`
+  userStepGoal.innerHTML = `Your Step Goal: ${currentUser.dailyStepGoal}`
+
+  stepGoalDisplay()
+  friendNames()
 }
 
-  function friendNames() {
-    const userFriends = user.friends
-    const findFriendsNames = userFriends.reduce((acc, friend) => {
-      const friendInfo = userRepo.getUserData(friend)
+  function displaySleepData(){
+  // sleepData.innerHTML += 
+  // `<li> You slept ${hoursSlept()}hours! yout sleep quality was ${sleepQuality(example)}</li>
+  // <li>You slept ${hoursSlept(example)}hours this week! yout sleep quality was ${sleepQuality}</li>
+  // <li>$ Your all-time average sleep quality is ${allTimeQuality(example)} and your all-time average number of hours slept is ${allTimeHoursSlept(example)}</li>
+  }
+
+function stepGoalDisplay() {     
+  averageStepGoal.innerHTML = `Your step goal is ${currentUser.dailyStepGoal} steps. The average step goal is ${userRepo.getAverageStepGoal()}.`
+}
+
+function friendNames() {
+  const userFriends = currentUser.friends
+  const findFriendsNames = userFriends.reduce((acc, friend) => {
+    const friendInfo = userRepo.getUserData(friend)
       acc += `<p>${friendInfo.name}'s step goal: ${friendInfo.dailyStepGoal}</p>`
       return acc
     }, "")
     friendsData.innerHTML = findFriendsNames
   }
-
-
-
